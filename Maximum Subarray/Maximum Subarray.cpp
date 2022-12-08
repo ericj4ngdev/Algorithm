3b1dda92665ed3b65e6f511e9d85e11c0fa45243
ericjangndev <ericjangdev@gmail.com>
2022-12-08 22:31:33 +0900

    Maximum Subarray

---
+++ Maximum Subarray/Maximum Subarray.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int j = 0; j < N; j++)
+        {
+            cin >> arr[j];
+        }
+        int psum = 0;
+        int max_val = -999;
+        for (int i = 0; i < N; i++) {
+            psum = max(0, psum) + arr[i];
+            max_val = max(max_val, psum);
+        }
+        cout << max_val << endl;
+    }
+
+    return 0;
+}
