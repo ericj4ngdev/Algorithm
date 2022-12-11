70f7de9812c2e4aa068ec8d71c401ca60e2ef902
ericjangndev <ericjangdev@gmail.com>
2022-12-11 20:56:39 +0900

    Test

---
+++ LongestIncreasingSubsequence/LongestIncreasingSubsequence.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int R() {
+	int N = 10;
+	return  rand() % N;
+}
+
+int main()
+{
+	int M = 20;
+	vector<int> mini(20,0);
+	srand((unsigned int)time(NULL));
+	for(int i = 0 ; i < M ; i++)
+		mini[i] = min(R(), R(), R());
+
+    return 0;
+}
