363acb7b044681b599d147a54b30a0085a109db2
ericjangndev <ericjangdev@gmail.com>
2022-12-12 15:21:07 +0900

    ConectedComponent

--- LongestIncreasingSubsequence/LongestIncreasingSubsequence.cpp
+++ LongestIncreasingSubsequence/LongestIncreasingSubsequence.cpp
@@ -1,20 +1,41 @@
 ﻿#include <iostream>
-#include <vector>
+#include <algorithm>
 using namespace std;
 
+int length[1000];
+int arr[1000];
 
-int R() {
-	int N = 10;
-	return  rand() % N;
-}
+int main() {
 
-int main()
-{
-	int M = 20;
-	vector<int> mini(20,0);
-	srand((unsigned int)time(NULL));
-	for(int i = 0 ; i < M ; i++)
-		mini[i] = min(R(), R(), R());
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int result = 0;
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int k = 0; k < n; k++) {
+        length[k] = 1;  // 끝값
+        for (int i = 0; i < k; i++) // k보다 왼쪽에 있는 arr값 순회
+        {
+            if (arr[i] < arr[k]) // 자기보다 작으면
+            {
+                length[k] = max(length[k], length[i] + 1);
+                // i번째 인덱스에서 끝나는 최장 증가 부분 수열의 마지막 + arr[k]를 추가했을 때의 LIS 길이
+                // 추가하지 않은 기존은 length[k]
+            }
+        }
+        result = max(result, length[k]);
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << length[i] << " ";
+
+    cout << endl;
+    cout << n - result;
 
     return 0;
 }
--- algorithm_Lesson/algorithm_Lesson.cpp
+++ algorithm_Lesson/algorithm_Lesson.cpp
@@ -1,38 +1,20 @@
-﻿// algorithm_Lesson.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
 using namespace std;
 
-int search(int a[], int n, int x);
+
+int R() {
+	int N = 10;
+	return  rand() % N;
+}
 
 int main()
 {
-	int result = 0;
-	int a[7] = { 1,2,3,4,5,6,7 };
-	int x = 0;
-
-	cin >> x;
-
-	result = search(a, 7, x);
-
-	cout << "찾으려는 값 "<< x <<"은(는) " << result + 1 << "번째에 있습니다.";
+	int M = 20;
+	vector<int> mini(20, 0);
+	srand((unsigned int)time(NULL));
+	for (int i = 0; i < M; i++)
+		mini[i] = min(R(), R(), R());
 
 	return 0;
-}
- 
-
-int search(int a[], int n, int x)
-{
-	int l, r, m;
-	l = 0;
-	r = n - 1;
-	while (l <= r)
-	{
-		m = (l + r) / 2;
-		if (a[m] == x)return m;
-		else if (a[m] > x)r = m - 1;
-		else l = m + 1;
-	}
-	return -1;
 }
