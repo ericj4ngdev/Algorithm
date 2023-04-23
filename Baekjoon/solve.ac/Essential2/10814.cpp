2726fa47637c28fc22ad2439897c443db29cbbf1
ericjangdev_HomeDeskTop <ericjangdev@gmail.com>
2023-04-23 23:33:42 +0900

    10814

---
+++ Baekjoon/solve.ac/Essential2/10814.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool compare(pair<int,string> a, pair<int,string> b)
+{
+    return a.first < b.first;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int num;
+    cin >> num;
+    pair<int,string> tmp;
+    vector<pair<int,string>> arr;
+    for(int i = 0; i < num; i++)
+    {
+        cin >> tmp.first >> tmp.second;
+        arr.push_back(tmp);
+    }
+    stable_sort(arr.begin(),arr.end(),compare);
+    for(int i = 0; i < num; i++)
+        cout << arr[i].first << ' ' << arr[i].second << '\n';
+}
