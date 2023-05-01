d52b62ff59735178fc3103b64a68cbfba00e19f5
VRLab_Com <ericjangdev@gmail.com>
2023-05-01 10:03:55 +0900

    2439

---
+++ Baekjoon/2439.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(){
+    int count; std::cin >> count;
+    for (int i = 0; i < count; i++)
+    {
+        for (int k = count-1; k > i; k--)
+            std::cout << " ";        
+        
+        for (int j = 0; j <= i; j++)
+            std::cout << "*";
+          
+        std::cout << "\n";
+    }
+    return 0;
+}
