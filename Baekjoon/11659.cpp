bbeb3f97f67e155941491dead27e5de1f1ea1d9b
ericjangdev_HomeDeskTop <ericjangdev@gmail.com>
2023-05-18 00:17:00 +0900

    11659

---
+++ Baekjoon/11659.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M; 
+    cin >> N >> M;
+    int S[100001] = {};
+
+    for (int i = 1; i <= N; i++)
+    {
+        int input;
+        cin >> input; 
+        S[i] = S[i-1] + input;
+    }
+    
+    for (int i = 0; i < M; i++)
+    {
+        int start, end;
+        cin >> start >> end;
+        cout << S[end] - S[start-1] << '\n';
+    }
+}
